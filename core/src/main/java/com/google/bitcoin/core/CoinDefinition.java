76a391ebaa9f4298937eb615c5f72130cba7a438
moondoggo <anthony@sureazure.com>
2018-04-12 23:16:18 -0700

    update version strings to 0.12.4

--- core/src/main/java/com/google/bitcoin/core/CoinDefinition.java
+++ core/src/main/java/com/google/bitcoin/core/CoinDefinition.java
@@ -112,7 +112,7 @@ public class CoinDefinition {
     //taken from the raw data of the block explorer
     static public String genesisXInBytes = "04";   //"BIN COIN START"
     static public String genessiXOutBytes = "04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f";
-    
+
     static public String[] dnsSeeds = new String[] {
             "seed.dime.life"
     };
